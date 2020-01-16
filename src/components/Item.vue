1c472681ac7385ff8696bc4b95270406dfeb038c
Avior Aharon <47120205+aviorp@users.noreply.github.com>
2020-01-16 13:51:07 +0200

    button disable attr added

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,52 +1,55 @@
 <template>
-<v-col>
-    <v-card  dark :color="color" width="400px">
-        <v-card-title>{{title}}</v-card-title>
-        <v-card-subtitle>(Price :{{price}} <span v-if="quantity">,  Quantity: {{quantity}} </span>)  </v-card-subtitle>
-        <hr>
-        <v-card-text style="background-color:white;">
-            <v-row>
-                <v-col cols="9">
-                    <v-text-field light solo label="Quantity" v-model="productQuantity" dense text></v-text-field>
-                </v-col>
-                <v-col>
-                    <v-btn light @click="btnFunction({quantity:productQuantity ,index}) ; productQuantity=''">{{btn}}  </v-btn>
-                </v-col>
-            </v-row>
-        </v-card-text>
+  <v-col>
+    <v-card dark :color="color" width="400px">
+      <v-card-title>{{title}}</v-card-title>
+      <v-card-subtitle>
+        (Price :{{price}}
+        <span v-if="quantity">, Quantity: {{quantity}}</span>)
+      </v-card-subtitle>
+      <hr />
+      <v-card-text style="background-color:white;">
+        <v-row>
+          <v-col cols="9">
+            <v-text-field light solo label="Quantity" v-model="productQuantity" dense text></v-text-field>
+          </v-col>
+          <v-col>
+            <v-btn
+              light
+              :disabled="productQuantity <=0"
+              @click="btnFunction({quantity:productQuantity ,index}) ; productQuantity=''"
+            >{{btn}}</v-btn>
+          </v-col>
+        </v-row>
+      </v-card-text>
     </v-card>
-    </v-col>
+  </v-col>
 </template>
 
 <script>
-    export default {
-        props: {
-            title: String,
-            price: Number,
-            color:String,
-            quantity:Number,
-            id:Number,
-            btn:String,
-            index:Number,
-            btnFunction:Function,
-           },
-           data() {
-               return {
-                  productQuantity : null
-               }
-           },
-           updated(){
-         
-               
-           },
-           methods: {
-               showId(id) {
-                 console.log(id)      
-               }
-           },
+export default {
+  props: {
+    title: String,
+    price: Number,
+    color: String,
+    quantity: Number,
+    id: Number,
+    btn: String,
+    index: Number,
+    btnFunction: Function
+  },
+  data() {
+    return {
+      productQuantity: null
+    };
+  },
+  updated() {},
+  methods: {
+    showId(id) {
+      console.log(id);
     }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
